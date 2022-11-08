26ac3a7605858926c49243d9512666c783b8c3c3
Nicolas Schmid <nicolas@breiten.ch>
2022-11-08 14:49:11 +0100

    feat: tidy up loss endpoints file

--- app/routers/loss.py
+++ app/routers/loss.py
@@ -1,15 +1,11 @@
-import time
-from typing import List
-
-from esloss.datamodel import EEarthquakeType, ELossCategory
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from esloss.datamodel import ELossCategory
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.dependencies import get_db
-from app.schemas import (AggregatedLossSchema, AggregationTagSchema,
-                         ELossStatistics, LossStatisticsSchema)
+from app.schemas import (AggregationTagSchema, ELossStatistics,
+                         LossStatisticsSchema)
 
 router = APIRouter(prefix='/loss', tags=['loss'])
 
@@ -25,24 +21,18 @@ async def get_losses(losscalculation_id: int,
     """
     Returns a list of all realizations of loss for a calculation.
     """
-    now = time.perf_counter()
     db_result = crud.read_tag_losses_df(db, losscalculation_id, 'd')
 
     if db_result.empty:
         raise HTTPException(status_code=404, detail="No loss found.")
 
-    print(time.perf_counter()-now)
-    print(db_result)
-
-    now = time.perf_counter()
     db_result = crud.read_aggregation_losses_df(db, losscalculation_id,
                                                 aggregation_type,
                                                 losscategory,
                                                 aggregationtag)
     if db_result.empty:
         raise HTTPException(status_code=404, detail="No loss found.")
-    print(time.perf_counter()-now)
-    print(db_result)
+
     return None
 
 
@@ -58,15 +48,11 @@ async def get_mean_losses(losscalculation_id: int,
     """
     Returns a list of statistical measures for the available aggregations.
     """
-    now = time.perf_counter()
     db_result = crud.read_mean_losses_df(
         db, losscalculation_id, aggregation_type, losscategory, aggregationtag)
 
     if db_result.empty:
         raise HTTPException(status_code=404, detail="No loss found.")
-
-    print(time.perf_counter()-now)
-    print(db_result)
 
     return [LossStatisticsSchema(loss={'value': 0},
                                  losscategory=ELossCategory.STRUCTURAL,
