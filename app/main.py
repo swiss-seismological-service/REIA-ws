fe2191dca666b27c323c38063911c1ea40d2cfe4
Nicolas Schmid <nicolas@breiten.ch>
2022-12-11 22:50:57 +0100

    feat: add aggregationtag and risktype partition query

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from reia.datamodel.base import ORMBase
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from reia.datamodel.base import ORMBase
 
 from app.database import engine
 from app.routers import calculations, damage, loss
 
 ORMBase.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(root_path='/riaws')
 app.include_router(loss.router, prefix='/v1')
 app.include_router(damage.router, prefix='/v1')
 app.include_router(calculations.router, prefix='/v1')
