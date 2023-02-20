e9953265cd97e95e5c728a692f9c78db58f9c79c
Nicolas Schmid <nicolas@breiten.ch>
2023-02-20 20:00:14 +0100

    feat: add index creation to script

--- db/init_function.sh
+++ db/init_function.sh
@@ -8,4 +8,5 @@ set +a
 psql -d $DB_NAME -U $DB_USER --host $POSTGRES_HOST  -f $(pwd)/db/functions/materialized_loss_buildings.sql \
                                                     -f $(pwd)/db/functions/trigger_refresh_materialized.sql \
                                                     -f $(pwd)/db/functions/trigger_partition_aggregationtags.sql \
-                                                    -f $(pwd)/db/functions/trigger_partition_losstype.sql
+                                                    -f $(pwd)/db/functions/trigger_partition_losstype.sql \
+                                                    -f $(pwd)/db/functions/indexes.sql \
