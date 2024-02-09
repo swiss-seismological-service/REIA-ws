079566816de27b09e4caf3ef5588d658bd7e6532
Nicolas Schmid <nicolas@breiten.ch>
2024-02-09 15:38:07 +0100

    fix: config settings correctly for updated pydantic_settings

--- config/config.py
+++ config/config.py
@@ -1,10 +1,12 @@
 import enum
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     POSTGRES_USER: str
@@ -28,10 +30,6 @@ class Settings(BaseSettings):
             f"{self.POSTGRES_PASSWORD}@" \
             f"{self.POSTGRES_HOST}:" \
             f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
-    class Config:
-        env_file = ".env"
-        extra = "ignore"
 
 
 @lru_cache()
